fc278e8af05d3f714ef1e98672b0d9ec2708ee2b
Henkho Dryza <simangahk@gmail.com>
2020-04-14 08:17:25 -0700

    created the first models

--- source/website/models.py
+++ source/website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
