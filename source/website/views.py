4ff7a8cbe0ff652e0fd64ffbbc69bbaea024fdd3
Henkho Dryza <simangahk@gmail.com>
2020-04-14 12:53:32 -0700

    added the form ui

--- source/website/views.py
+++ source/website/views.py
@@ -5,3 +5,7 @@ from .models import Member
 def home(request):
     all_members = Member.objects.all
     return render(request, 'home.html', {'all_members': all_members})
+
+
+def join(request):
+    return render(request, 'join.html', {})
