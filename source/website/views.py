b518e5dcf85d4aeaf2a9122b2fd3d1bcc1e725ab
Henkho Dryza <simangahk@gmail.com>
2020-04-15 13:39:35 -0700

    redirected after sign up to home

--- source/website/views.py
+++ source/website/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Member
 from .forms import MemberForm
+from django.contrib import messages
 
 
 def home(request):
@@ -13,7 +14,9 @@ def join(request):
         form = MemberForm(request.POST or None)
         if form.is_valid():
             form.save()
-            all_members = Member.objects.all
-            return render(request, 'home.html', {'all_members': all_members})
+            messages.success(
+                request, ('You are succeful added to membership!'))
+
+            return redirect('home')
     else:
         return render(request, 'join.html', {})
