49a3963f1db2aff6bd0c23cec9bb8000e484c573
Henkho Dryza <simangahk@gmail.com>
2020-05-21 00:13:44 -0700

    deleted the website app

--- source/website/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Member
-
-
-class MemberForm(forms.ModelForm):
-    class Meta:
-        model = Member
-        fields = ['first_name', 'last_name', 'email', 'password', 'age']
