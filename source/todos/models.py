35147aa08f719d0c33b59e291cb05f785d25cd00
Henkho Dryza <simangahk@gmail.com>
2020-06-02 08:26:02 -0700

    improved todo pages

--- source/todos/models.py
+++ source/todos/models.py
@@ -5,7 +5,7 @@ import datetime
 class Todo(models.Model):
     title = models.CharField(max_length=30, null=False)
     description = models.CharField(max_length=1000, null=False)
-    complete = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False, null=False)
     created_date = models.DateTimeField(default=datetime.datetime.now)
 
     def __str__(self):
--- source/todos/views.py
+++ source/todos/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoForm
+from django.contrib import messages
 
 
 def todos(request):
-    return render(request, 'todos/todos.html')
+    if request.method == 'POST':
+        form = TodoForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Todo successful created"))
+
+            return redirect('todos')
+
+    if request.method == 'GET':
+        all_todos = Todo.objects.all
+        return render(request, 'todos/todos.html', {'all_todos': all_todos})
