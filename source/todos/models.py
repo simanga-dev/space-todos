d94c2051d6f3a0a981d2c846c1a8e0b73db4dca2
Henkho Dryza <simangahk@gmail.com>
2020-06-04 03:44:27 -0700

    re created databse

--- source/todos/models.py
+++ source/todos/models.py
@@ -5,8 +5,9 @@ import datetime
 class Todo(models.Model):
     title = models.CharField(max_length=30, null=False)
     description = models.CharField(max_length=1000, null=False)
-    complete = models.BooleanField(default=False, null=False)
-    created_date = models.DateTimeField(default=datetime.datetime.now)
+    isComplete = models.BooleanField(default=False, null=False)
+    date_created = models.DateField(default=datetime.date.today)
+    time_created = models.TimeField(default=datetime.datetime.now)
 
     def __str__(self):
         return self.title
