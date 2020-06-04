75586a6fa96e40e368569891a70739926bfcb547
Henkho Dryza <simangahk@gmail.com>
2020-06-04 01:37:54 -0700

    added notification

--- source/todos/urls.py
+++ source/todos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todos, name='todos')
+    path('', views.todos, name='todos'),
+    path('delete/<todo_id>', views.delete, name='delete')
 ]
--- source/todos/views.py
+++ source/todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Todo
 from .forms import TodoForm
 from django.contrib import messages
@@ -6,13 +6,25 @@ from django.contrib import messages
 
 def todos(request):
     if request.method == 'POST':
+        print(request.body)
         form = TodoForm(request.POST or None)
         if form.is_valid():
             form.save()
             messages.success(request, ("Todo successful created"))
 
+            # return HttpResponse("todos was added")
             return redirect('todos')
+        else:
+            messages.error(request, ("Please provide all information"))
+            return HttpResponse("inavlid form")
 
     if request.method == 'GET':
         all_todos = Todo.objects.all
         return render(request, 'todos/todos.html', {'all_todos': all_todos})
+
+def delete(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.delete()
+    messages.success(request, ("Todo has been deleted!"))
+    # return redirect('todos')
+    return HttpResponse("Todo has been deleted")
